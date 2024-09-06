30ee00065a9c0480c581ff39c43097825b44bc08
Asjad Abrar <asjadabr40@gmail.com>
2024-09-06 23:28:10 +0500

    gyufyufyugu

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from '../../components/home/hero-section/hero-section.component';
+import { ReasonsComponent } from '../../components/home/reasons/reasons.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroSectionComponent],
+  imports: [HeroSectionComponent,ReasonsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
