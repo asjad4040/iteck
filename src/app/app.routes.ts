a292d58d8970bb3e46ccaa46bf484326fb562929
Asjad Abrar <asjadabr40@gmail.com>
2024-09-06 11:38:02 +0500

    hero section of home page done in mobile view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,7 +13,7 @@ export const routes: Routes = [
         component:SassTechComponent,
         children:[
             {
-                path:'home',
+                path:'',
                 component:HomeComponent
             }
         ]
